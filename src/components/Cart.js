1c899284254c99addd8926c51f8b7c44cbbcd0c5
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-07-11 17:55:25 +0200

    config: cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import products from '../products'
+import CartItem from './CartItem'
+import '../Cart.css'
+import {MdRemoveShoppingCart} from 'react-icons/md'
 
 const Cart = () => {
   return (
-    <div>
-      Cart
-    </div>
-  )
-}
+    <section style={{ margin: '2rem' }}>
+      <div className='row'>
+        <div className='col-12 d-flex justify-content-between align-items-center custom-gap'>
+          <h6 className='flex-grow-1 text-center'>Item</h6>
+          <h6 className='flex-grow-1 text-center'>Nome</h6>
+          <h6 className='flex-grow-1 text-center'>Qty</h6>
+          <h6 className='flex-grow-1 text-center'>Prezzo</h6>
+          <button className='btn btn-danger'>
+            <MdRemoveShoppingCart />
+          </button>
+        </div>
+      </div>
+      <hr />
+      <section>
+        {
+          products.map((el => {
+            return (
+              <CartItem key={el._id} {...el} />
+            )
+          }))
+        }
+      </section>
+    </section>
+  );
+};
 
 export default Cart
